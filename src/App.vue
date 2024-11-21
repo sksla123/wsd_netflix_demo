813422fce66096173fe09a193250a048cba75505
sksla123 <pjy010608@naver.com>
2024-11-21 14:58:51 +0000

    헤더

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,146 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <!-- <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link>
-    </nav>
-  </div> -->
+  <header class="responsive-header">
+    <div class="header-left">
+      <a href="#" class="logo">넷플릭스</a>
+      <nav class="desktop-nav">
+        <a href="#">홈</a>
+        <a href="#">지금 뜨는 콘텐츠</a>
+        <a href="#">찾아보기</a>
+        <a href="#">내가 찜한 리스트</a>
+      </nav>
+    </div>
+    <div class="header-right">
+      <div class="profile-pic" @click="toggleProfile">👤</div>
+      <div class="mobile-menu" @click="toggleMenu">☰</div>
+    </div>
+    <div v-if="showMenu" class="mobile-nav">
+      <div class="mobile-profile">
+        <img :src="profileImage" alt="Profile" />
+        <p>간단한 프로필 소개</p>
+      </div>
+      <nav>
+        <a href="#">홈</a>
+        <a href="#">지금 뜨는 콘텐츠</a>
+        <a href="#">찾아보기</a>
+        <a href="#">내가 찜한 리스트</a>
+      </nav>
+    </div>
+  </header>
   <router-view></router-view>
 </template>
 
-<style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false,
+      profileImage: 'path_to_profile_image.jpg' // 프로필 이미지 경로 설정 필요
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
+    },
+    toggleProfile() {
+      // 프로필 클릭 시 추가 동작 구현
+    }
+  }
+};
+</script>
 
-nav {
-  padding: 30px;
+<style scoped>
+.responsive-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background: #141414;
+  position: relative;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.header-left {
+  display: flex;
+  align-items: center;
 }
 
-nav a.router-link-active {
-  color: #42b983;
+.logo {
+  color: #e50914;
+  font-size: 24px;
+  text-decoration: none;
+  margin-right: 20px;
+}
+
+.desktop-nav {
+  display: flex;
+}
+
+.desktop-nav a {
+  color: white;
+  margin: 0 15px;
+  text-decoration: none;
+}
+
+.header-right {
+  display: flex;
+  align-items: center;
+}
+
+.profile-pic, .mobile-menu {
+  cursor: pointer;
+  color: white;
+  font-size: 20px;
+  margin-left: 20px;
+}
+
+.mobile-menu {
+  display: none;
+}
+
+.mobile-nav {
+  display: none;
+  flex-direction: column;
+  background: #141414;
+  position: absolute;
+  top: 100%;
+  right: 0;
+  width: 200px;
+  border: 1px solid #e50914;
+}
+
+.mobile-profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #212121;
+  padding: 10px;
+}
+
+.mobile-profile img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-bottom: 10px;
+}
+
+.mobile-nav a {
+  color: white;
+  padding: 10px;
+  text-decoration: none;
+}
+
+@media (max-width: 768px) {
+  .desktop-nav {
+    display: none;
+  }
+
+  .mobile-menu {
+    display: block;
+  }
+
+  .mobile-nav {
+    display: flex;
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../components/Home.vue'
-import Login from '../components/Login.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  }
 ]
 
 const router = createRouter({
