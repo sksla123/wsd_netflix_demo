402759603f996c5a189dd9e8f575dd1f41a24a4c
sksla123 <pjy010608@naver.com>
2024-11-24 11:11:22 +0000

    fabicon

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import './index.css';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSpinner, faStar } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSpinner, faStar)
 
 const app = createApp(App)
 app.use(store)
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
